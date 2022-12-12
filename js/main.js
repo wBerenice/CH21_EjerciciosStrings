764db84c4c7e99e0fa8d1b4d7e859c5cc61a23c9
BereniceSan <sanchezberenice09@gmail.com>
2022-12-12 17:17:49 -0600

    Se realizaron validaciones de un textArea y un correo electrónico

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 //Ejercicios STRINGS
 
 
-
 //Ejercicio 01
-let nombre = "Valeria Duque";
+let nombre = "Laura Aline";
 nombre = nombre.toUpperCase();
 let letra = "";
 let contFinal = 0;
@@ -26,30 +25,39 @@ console.log(letra, contFinal);
 
 
 
+let btnEnviar = document.getElementById("btnEnviar");
 
+btnEnviar.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
+    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
+    let exampleFormControlTextarea1 = document.getElementById("exampleFormControlTextarea1");
+    let alertError = document.getElementById("alertError");
+    exampleFormControlTextarea1.value = exampleFormControlTextarea1.value.trim();
+    alertError.style.display = "none";
+    alertError.innerHTML = "";
 
-// let contador = 0;
+    console.log(exampleFormControlTextarea1.value.length);
+    console.log("[" + exampleFormControlTextarea1.value.replaceAll("  ", "") + "]");
 
-// function caracterRepetido(){
+    
+    
+    console.log(exampleFormControlTextarea1.value.replaceAll("  ", "").length);
+    if(exampleFormControlTextarea1.value.trim().replaceAll("  ", "").length < 20){
+        alertError.innerHTML = `El mensaje debe contener 20 caracteres o más`;
+        alertError.style.display = "block";
+        exampleFormControlTextarea1.focus();
+        exampleFormControlTextarea1.select();
+    }//if
 
-//     for(let i = 0; i < nombre.length; i++){
-//         console.log(i)
-//         let caracterActual = null;
+    if(exampleFormControlInput1.value.match(emailRegex) == null){
+        alertError.style.display = "block";
+        // alertError.innerHTML += (`<br/>El correo electrónico no es válido`)
+        alertError.innerHTML += "<br/>El correo electrónico no es válido"
+    }
 
-//         if(caracterActual == null){
-//             caracterActual = nombre[i]
-//         }else if(caracterActual = nombre[i]){
+});
 
-//         }
-
-
-
-//     }
-
-// }
-
-// console.log(nombre);
-
-// caracterRepetido();
 
